7d44cd775e506ddb50e4a8060453295fc29bd2d9
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-07-19 23:49:42 +0300

    Add start file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Framework/core/Route.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
+
+$router= new Route();
+$router->run();
+
+
