5bad3a0bad58ae08aea57dbcbcb831cad4ca6b62
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-07-22 18:30:44 +0300

    Add new pages

---
+++ App/views/pages/registration.php
@@ -0,0 +1,28 @@
+<main class="main-autoriz container">
+		<section>
+				<form>
+						<div class="input-wrap">
+								<input type="text" placeholder="Введите ваше имя" name="login" class="input" >
+						</div>
+						<div class="input-wrap">
+								<input type="text" placeholder="Введите вашу фамилию" name="password"  class="input" >
+						</div>
+						<div class="input-wrap">
+								<input type="text" placeholder="Введите ваш логин" name="password"  class="input" >
+						</div>
+						<div class="input-wrap">
+								<input type="password" placeholder="Введите пароль" name="password"  class="input" >
+						</div>
+						<div class="input-wrap">
+								<input type="email" placeholder="Введите вашу почту" name="password"  class="input" >
+						</div>
+						<div class="input-wrap button-wrap">
+								<button>Регистрация</button>
+						</div>
+				</form>
+		</section>
+		<section class="lumen-about">
+				<h1 class="main-title">Дизайнерские люстры от мировых брендов</h1>
+				<p class="info-main">Сделайте интерьер стильным и современным</p>
+		</section>
+</main>
