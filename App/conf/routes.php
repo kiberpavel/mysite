44247da82f20bfd05ffdcfa58516bfa47585eee7
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-07-22 18:18:02 +0300

    Add to roades file new route

--- App/conf/routes.php
+++ App/conf/routes.php
@@ -1,10 +1,10 @@
 <?php
-
 return array(
     'index' =>'index/index',
     'catalog' =>'catalog/catalog',
     'about' =>'about/about',
     'login' =>'login/login',
     'basket' =>'basket/basket',
+    'cabinet'=>'cabinet/cabinet',
     'registration'=>'registration/reg',
 );
