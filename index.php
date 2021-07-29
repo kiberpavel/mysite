3b925e730c44586e2d39d208dd439b834fa17de7
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-07-29 20:06:11 +0300

    Detele include

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-require_once($_SERVER['DOCUMENT_ROOT'] . '/Framework/core/Route.php');
 include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
 
 $router= new Route();
