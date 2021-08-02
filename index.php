309965c3c94f9474a107acfee5c2cd6e5a71401e
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-02 12:41:37 +0300

    Add logger in index

--- index.php
+++ index.php
@@ -7,6 +7,16 @@ ini_set('display_errors', 'On');
 //include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
 require ($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('App/Log/log.log', Logger::WARNING));
+
+// add records to the log
+$log->warning('Warning');
+$log->error('Error');
+
 $router= new Core\Route();
 $router->run();
 
