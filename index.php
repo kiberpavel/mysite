68a498af7f963c18559b5d117e5c1f47459f96ae
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-02 12:32:40 +0300

    Add new autoload in index

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
+
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
+//include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
+require ($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
-$router= new Route();
+$router= new Core\Route();
 $router->run();
-
 
