d9365fe586875492b2e011d933dc16397f6173b3
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 01:30:35 +0300

    Add checker fo validate

--- App/models/RegistrationModel.php
+++ App/models/RegistrationModel.php
@@ -1,7 +1,56 @@
 <?php
+
 namespace Models;
-use Core\Model;
 
-class RegistrationModel extends Model{
+class RegistrationModel
+{
+    public static function checkName($name)
+    {
+        if (strlen($name) >= 2) {
+            return true;
+        }
+        return false;
+    }
+    public static function checkSecondName($second_name)
+    {
+        if (strlen($second_name) >= 2) {
+            return true;
+        }
+        return false;
+    }
+    public static function checkLogin($login)
+    {
+        if (strlen($login) >= 4) {
+            return true;
+        }
+        return false;
+    }
+    public static function checkPassword($password)
+    {
+        if (strlen($password) >= 4) {
+            return true;
+        }
+        return false;
+    }
+    public static function checkEmail($email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+        return false;
+    }
 
+//    public function checkType(): bool
+//    {
+//        if (isset($_POST['submit'])) {
+//            $name = $_POST['name'];
+//            $second_name = $_POST['second_name'];
+//            $login = $_POST['login'];
+//            $password = $_POST['password'];
+//            $email = $_POST['email'];
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 }
