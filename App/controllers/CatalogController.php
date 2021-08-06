80e10d401fee2c68f5154f736225aa21cb94b60c
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 01:29:40 +0300

    Add connection to database for conclusion items

--- App/controllers/CatalogController.php
+++ App/controllers/CatalogController.php
@@ -1,24 +1,29 @@
 <?php
+
 namespace Controllers;
+
 use Core\Controller;
-use Core\View;
-use Models\CatalogModel;
-class CatalogController extends Controller{
-    
-    public $model;
-    public $view;
-    
+
+class CatalogController extends Controller
+{
     public function __construct()
     {
         parent::__construct();
-        $itemList = require_once($_SERVER['DOCUMENT_ROOT'] . '/App/conf/db.php');
-        $this->model = new CatalogModel($itemList);
-        $this->view = new View();
     }
-    
-    public function actionCatalog(){
-        $title = "Каталог";
-            $itemList= $this->model->getProducts();
-        $this->view->render('catalog',['itemList' =>$itemList, 'title' =>$title ]);
+
+    public function actionCatalog()
+    {
+        $params = ['itemList' => $this->items->selectAll(),
+            'categoryList' => $this->items->getCategory(), 'title' => "Каталог"];
+        $this->view->render('catalog', $params);
+    }
+    public function actionCategory()
+    {
+        $arrUrl = explode('/', $_SERVER['REQUEST_URI']);
+        $category = ucfirst(end($arrUrl));
+        $params = ['itemList' => $this->items->findByCategory($category), 'title' => "Каталог"];
+        $this->view->render('catalog', $params);
+//        $test = $this->items->findByCategory($category);
+//        var_dump($test);
     }
 }
