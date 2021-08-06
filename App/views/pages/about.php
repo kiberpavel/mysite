9c9b5951a4aaffcabafda4ed7bbf58d638e3edda
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 01:32:01 +0300

    Edit routes

--- App/views/pages/about.php
+++ App/views/pages/about.php
@@ -1,17 +1,15 @@
 <main class="about-page">
   <section class="main-info container">
     <div class="about-photo">
-      <img src="../../../public/image/lamp-catalog.png" alt="Товар" width="" height="">
+      <img src="/public/image/<?= $photo ?>" alt="Товар" width="" height="">
     </div>
     <div class="about-info">
-		    <p class="about-name distance">Люстра</p>
+		    <p class="about-name distance"><?= $name ?></p>
       <p class="about-title">Описание</p>
-      <p class="about-text distance">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        Adipisci assumenda aut, deleniti doloribus eaque earum eum excepturi,
-        in ipsam molestiae nam omnis perferendis, placeat praesentium quas ut voluptatem? Maiores, necessitatibus. </p>
-      <p class="about-dop">Страна:Португалия</p>
-      <p class="about-dop distance">Бренд:KGnldsf</p>
-		    <p class="about-price ">2414грн</p>
+      <p class="about-text distance"><?= $about ?></p>
+      <p class="about-dop">Страна:<?= $country ?></p>
+      <p class="about-dop distance">Бренд:<?= $brend ?></p>
+		    <p class="about-price "><?= $price ?>грн</p>
 		    <div class="distance" >
 						    <button  class="about-button">Купить</button>
 		    </div>
