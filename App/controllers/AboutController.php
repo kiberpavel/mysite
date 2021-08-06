e6cd4869e8ae9839530d6b3826aa3eddfaf8f93f
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 01:29:25 +0300

    Add connection to database for conclusion item

--- App/controllers/AboutController.php
+++ App/controllers/AboutController.php
@@ -1,21 +1,31 @@
 <?php
-namespace Controllers;
-use Core\Controller;
-use Core\View;
-use Models\AboutModel;
 
-class AboutController extends Controller{
-    
-    
+namespace Controllers;
+
+use Core\Controller;
+
+class AboutController extends Controller
+{
     public function __construct()
     {
         parent::__construct();
-        $this->model = new AboutModel();
-        $this->view = new View();
     }
-    
-    public function actionAbout(){
-        $title = "О товаре";
-        $this->view->render('about',['title' =>$title]);
+
+    public function actionAbout()
+    {
+        $arrUrl = explode('/', $_SERVER['REQUEST_URI']);
+        $id = (int)end($arrUrl);
+        $item = $this->items->findById($id);
+        extract($item, EXTR_OVERWRITE);
+        $params = ['title' => "О товаре",
+            'id' => $id,
+            'name' => $name,
+            'about' => $about,
+            'photo' => $photo,
+            'country' => $country,
+            'brend' => $brend,
+            'price' => $price
+            ];
+        $this->view->render('about', $params);
     }
 }
