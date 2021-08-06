0d7d7973974299425a0f5e67248ed9842cb3a474
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 01:37:30 +0300

    Changing the arrangement of objects

--- index.php
+++ index.php
@@ -1,22 +1,42 @@
 <?php
 
-
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-//include($_SERVER['DOCUMENT_ROOT'] . '/Framework/helpers/autoload.php');
-require ($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('App/Log/log.log', Logger::WARNING));
 
-// add records to the log
-$log->warning('Warning');
-$log->error('Error');
+//$log = new Logger('name');
+//$log->pushHandler(new StreamHandler('App/Log/log.log', Logger::WARNING));
+//
+//// add records to the log
+//$log->warning('Warning');
+//$log->error('Error');
 
-$router= new Core\Route();
+//$host = 'localhost';
+//$dbname = 'butenko_db';
+//$username = 'butenko';
+//$password = '1000';
+
+//try {
+//    $db = Database::getConnection();
+//}
+//catch(PDOException $e) {
+//    echo $e->getMessage();
+//    exit();
+//}
+//echo "Подключение успешно установлено";
+
+//$item = new Items();
+//$item->selectAll();
+//
+//foreach ($item as $arr){
+//    print_r($arr);
+//        echo "<br>";
+//}
+
+$router = new Core\Route();
 $router->run();
-
