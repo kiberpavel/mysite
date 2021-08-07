26ed074f39e9b502b5db9bd78da6ba5ccc0e3f6b
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 23:38:21 +0300

    Add action sorting by category

--- App/controllers/CatalogController.php
+++ App/controllers/CatalogController.php
@@ -13,17 +13,23 @@ class CatalogController extends Controller
 
     public function actionCatalog()
     {
+        $categoryList = $this->items->getCategory();
         $params = ['itemList' => $this->items->selectAll(),
-            'categoryList' => $this->items->getCategory(), 'title' => "Каталог"];
+            'categoryList' => $categoryList, 'title' => "Каталог"];
         $this->view->render('catalog', $params);
     }
     public function actionCategory()
     {
+        $categoryList = $this->items->getCategory();
         $arrUrl = explode('/', $_SERVER['REQUEST_URI']);
         $category = ucfirst(end($arrUrl));
-        $params = ['itemList' => $this->items->findByCategory($category), 'title' => "Каталог"];
+        $link = strtolower($category);
+//        var_dump($cat);
+        $params = ['itemList' => $this->items->findByCategory($category), 'title' => "Каталог",'link' => $link,'categoryList' => $categoryList];
         $this->view->render('catalog', $params);
 //        $test = $this->items->findByCategory($category);
+//        $test = $this->items->findByCategory($category);
+//       var_dump($category);
 //        var_dump($test);
     }
 }
