76f307896106bd5bb8f7dfc6515e41eae71675a9
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-07 23:40:32 +0300

    Add action writing data in database

--- App/controllers/RegistrationController.php
+++ App/controllers/RegistrationController.php
@@ -16,7 +16,7 @@ class RegistrationController extends Controller
         $this->user = new User();
         $this->user->setDb($db);
     }
-
+    
     public function actionReg()
     {
         $name = '';
@@ -25,37 +25,39 @@ class RegistrationController extends Controller
         $password = '';
         $email = '';
         $result = false;
-
+        
         if (isset($_POST['submit'])) {
             $name = $_POST['name'];
             $second_name = $_POST['second_name'];
             $login = $_POST['login'];
             $password = $_POST['password'];
             $email = $_POST['email'];
-
+            
             $errors = false;
+            
+            $password = md5($password . "skajhagkbgw");
 
-            if (!RegistrationModel::checkName($name)) {
-                $errors[] = 'Неправильное имя';
-            }
-
-            if (RegistrationModel::checkSecondName($second_name)) {
-                $errors[] = 'Неправильная фамилия';
-            }
-
-            if (RegistrationModel::checkLogin($login)) {
-                {
-                    $errors[] = 'Неправильный логин';
-                }
-            }
-
-            if (RegistrationModel::checkPassword($password)) {
-                $errors[] = 'Неккоректны пароль';
-            }
-
-            if (RegistrationModel::checkEmail($email)) {
-                $errors[] = 'Неккоректный email';
-            }
+//            if (!RegistrationModel::checkName($name)) {
+//                $errors[] = 'Неправильное имя';
+//            }
+//
+//            if (RegistrationModel::checkSecondName($second_name)) {
+//                $errors[] = 'Неправильная фамилия';
+//            }
+//
+//            if (RegistrationModel::checkLogin($login)) {
+//                {
+//                    $errors[] = 'Неправильный логин';
+//                }
+//            }
+//
+//            if (RegistrationModel::checkPassword($password)) {
+//                $errors[] = 'Неккоректны пароль';
+//            }
+//
+//            if (RegistrationModel::checkEmail($email)) {
+//                $errors[] = 'Неккоректный email';
+//            }
 
             if ($errors == false) {
                 $result = $this->user->register($name, $second_name, $login, $password, $email);
@@ -63,8 +65,11 @@ class RegistrationController extends Controller
         }
 
 //        $check = RegistrationModel::checkType();
-
+            
+        
+        
         $title = "Регистрация";
         $this->view->render('registration', ['title' => $title, 'errors' => $errors, 'result' => $result]);
+        return true;
     }
 }
