fde1af83cdbda59f0885b049c50e7868d2411dac
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-10 17:18:22 +0300

    Create user cabinet

--- App/controllers/CabinetController.php
+++ App/controllers/CabinetController.php
@@ -1,21 +1,33 @@
 <?php
+
 namespace Controllers;
+
 use Core\Controller;
-use Core\View;
-use Models\CabinetModel;
+use Core\Session;
+use Db\Database;
+use Models\User;
 
-
-class  CabinetController extends Controller{
-    
+class CabinetController extends Controller
+{
     public function __construct()
     {
         parent::__construct();
-        $this->model= new CabinetModel();
-        $this->view = new View();
     }
-    
-    public function actionCabinet(){
-        $title = "Личный кабинет";
-        $this->view->render('cabinet',['title' =>$title]);
+
+    public function actionCabinet()
+    {
+        if ($this->person) {
+            header("Location: /login");
+        }
+        $password = $_POST['password'];
+        $password = md5($password . "skajhagkbgw");
+        $params = ['title' => 'Личный кабинет',
+            'person' => $this->person, 'user' => $this->userInfo,
+            'edit' => $this->user->updatePassword($password, $this->userInfo['login'])];
+        $this->view->render('cabinet', $params);
+//        $ses = new Session();
+//        var_dump($ses->getId());
+
+        return true;
     }
 }
