56031d2c12b1f98a48baef9ac4b075611ee76985
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-10 17:27:00 +0300

    Moving params to a parent constructor

--- Framework/core/Controller.php
+++ Framework/core/Controller.php
@@ -3,19 +3,30 @@
 namespace Core;
 
 use Db\Database;
-use Models\Items;
+use Models\User;
 
 class Controller
 {
+    public $user;
     public $items;
     public $model;
     public $view;
+    public $userInfo;
+    public $userId;
+    public $person;
 
     public function __construct()
     {
         $db = Database::getConnection();
-        $this->items = new Items();
-        $this->items->setDb($db);
+        $this->user = new User();
+        $this->user->setDb($db);
+        $this->autentif = new Authentication();
         $this->view = new View();
+        $userInfo = $this->autentif->getUser();
+        $this->person = $this->autentif->isGuest();
+        if (!$this->person) {
+            $userId = $userInfo['id'];
+            $this->userInfo = $this->user->findById($userId);
+        }
     }
 }
