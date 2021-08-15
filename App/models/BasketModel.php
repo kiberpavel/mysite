ff3f223412a72a64b1f367a8f45494ced3231339
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-16 00:28:07 +0300

    Addding methods for work with basket

--- App/models/BasketModel.php
+++ App/models/BasketModel.php
@@ -1,6 +1,56 @@
 <?php
-namespace Models;
-use Core\Model;
-class BasketModel extends Model{
 
+namespace Models;
+
+use Core\Model;
+
+use Core\Session;
+
+class BasketModel extends Model
+{
+    public function addProduct($id)
+    {
+        $_SESSION['products']['id'][] = intval($id);
+        $_SESSION['products']['count'] = count($_SESSION['products']['id']);
+    }
+
+
+    public function getProducts()
+    {
+        if (isset($_SESSION['products'])) {
+            return $_SESSION['products']['id'];
+        }
+        return false;
+    }
+    public function getProductsId()
+    {
+        if (isset($_SESSION['products'])) {
+            return $_SESSION['products']['id'];
+        }
+        return false;
+    }
+    public function getProductsCount()
+    {
+        if (isset($_SESSION['products'])) {
+            return $_SESSION['products']['count'];
+        }
+        return false;
+    }
+
+    public function totalPrice($array, $key)
+    {
+        if (!empty($array)) {
+            $a = array_column($array, $key);
+            $b = array_sum($a);
+        }
+        return $b;
+    }
+
+    public function clear()
+    {
+        if (isset($_SESSION['products'])) {
+            $session = new Session();
+            $session->delete('products');
+        }
+        }
 }
