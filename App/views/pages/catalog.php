762cce1cf075adc267ae36bacd17132e6eccfa38
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-19 21:44:07 +0300

    Edit js output items

--- App/views/pages/catalog.php
+++ App/views/pages/catalog.php
@@ -5,7 +5,7 @@
                   <li class="menu-item">
                           <p class="button-categoy">Разделы</p>
                           <ul class="sub-menu">
-                              <?php if (isset($categoryList) && isset($name)) : ?>
+                              <?php if (isset($categoryList)) : ?>
                                   <?php foreach ($categoryList as $category) : ?>
                                         <?php extract($category, EXTR_OVERWRITE); ?>
                                             <li><a href="/catalog/<?=strtolower($name) ?>"><?= $name ?></a></li>
@@ -14,21 +14,39 @@
                           </ul>
                   </li>
           </ul>
-                  <div class="product-wrap">
-              <?php foreach ($itemList as $item) : ?>
-                    <?php extract($item, EXTR_OVERWRITE); ?>
-                          <a class="wrap" href="/about/<?=$id?>">
-                                  <div class="img-catalog">
-                                          <img src="/public/image/<?= $photo ?>" alt="Товар" width="315" height="200">
-                                  </div>
-                                  <p><?= $name ?></p>
-                                  <div class="upper-catalog">
-                                          <p><?= $model ?></p>
-                                          <p class="price"><?= $price ?></p>
-                                  </div>
-                          </a>
-              <?php endforeach; ?>
-                  </div>
+          <div class="product-wrap">Loading...</div>
   </section>
 
 </main>
+<script>
+
+async function getInfo(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+function outputProducts(data, elem) {
+    let html = '';
+    document.querySelector(elem).innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        html = `
+                          <a class="wrap" href="/about/${data[i].id}">
+                                  <div class="img-catalog">
+                                      <img src="/public/image/${data[i].photo}" alt="Товар" width="315" height="200">
+                                  </div>
+                                  <p>${data[i].name}</p>
+                                  <div class="upper-catalog">
+                                          <p>${data[i].model}</p>
+                                          <p class="price">${data[i].price}</p>
+                                  </div>
+                          </a>
+                          
+                        `;
+        document.querySelector(elem).innerHTML += html.trim();
+    }
+}
+
+getInfo(`api/catalog`)
+    .then((data) => outputProducts(data, '.product-wrap'))
+    .catch(err => console.log(err))
+
+</script>
