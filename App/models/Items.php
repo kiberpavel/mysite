733cda0d571fc6106494cf6c005c1edf14df26d0
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-19 20:00:57 +0300

    Edit sql for work

--- App/models/Items.php
+++ App/models/Items.php
@@ -24,9 +24,9 @@ class Items extends Model
 
     public function selectAll()
     {
-        $sth = self::$db->prepare("SELECT * FROM Items INNER JOIN Category on Items.id_category=Category.id");
+        $sth = self::$db->prepare("SELECT Items.id,Items.id_category,model,photo,about,country,brend,price,Category.name FROM Items INNER JOIN Category on Items.id_category=Category.id");
         $sth->execute();
-        $this->items = $sth->fetchAll();
+        $this->items = $sth->fetchAll(PDO::FETCH_ASSOC);
         return $this->items;
     }
 
@@ -35,22 +35,22 @@ class Items extends Model
         $sth = self::$db->prepare("SELECT * FROM Items INNER JOIN Category on Items.id_category=Category.id WHERE Items.id = :id");
         $sth->bindParam(':id', $id);
         $sth->execute();
-        $this->items = $sth->fetch();
+        $this->items = $sth->fetch(PDO::FETCH_ASSOC);
         return $this->items;
     }
     public function findByCategory(string $name)
     {
-        $sth = self::$db->prepare("SELECT * FROM Items INNER JOIN Category on Items.id_category=Category.id WHERE Category.name = :name");
+        $sth = self::$db->prepare("SELECT * FROM Items INNER JOIN Category on Items.id_category=Category.id_category WHERE Category.name = :name");
         $sth->bindParam(':name', $name);
         $sth->execute();
         $this->items = $sth->fetchAll();
         return $this->items;
     }
-    public function getCategory()
+    public function getCategories()
     {
         $sth = self::$db->prepare("SELECT * FROM  Category ");
         $sth->execute();
-        $this->items = $sth->fetchAll();
+        $this->items = $sth->fetchAll(PDO::FETCH_ASSOC);
         return $this->items;
     }
 
