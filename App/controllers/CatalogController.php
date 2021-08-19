c85eda21a11a0732dc5efc95bb96462069244cb6
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-19 19:57:47 +0300

    Do code more beautiful

--- App/controllers/CatalogController.php
+++ App/controllers/CatalogController.php
@@ -10,6 +10,7 @@ use Models\Items;
 
 class CatalogController extends Controller
 {
+    public Items $items;
     public function __construct()
     {
         parent::__construct();
@@ -20,19 +21,26 @@ class CatalogController extends Controller
 
     public function actionCatalog()
     {
-        $categoryList = $this->items->getCategory();
-        $params = ['itemList' => $this->items->selectAll(),
-            'categoryList' => $categoryList, 'title' => "Каталог",
-            'person' => $this->person, 'user' => $this->userInfo, 'count' => $this->count ];
+        $categoryList = $this->items->getCategories();
+        $params = [
+            'itemList' => $this->items->selectAll(),
+            'categoryList' => $categoryList,
+            'title' => "Каталог",
+            'person' => $this->person,
+            'user' => $this->userInfo,
+            'count' => $this->count
+            ];
+//        var_dump($this->items->selectAll());
+//        exit();
         $this->view->render('catalog', $params);
     }
     public function actionCategory()
     {
-        $categoryList = $this->items->getCategory();
+        $categoryList = $this->items->getCategories();
         $arrUrl = explode('/', $_SERVER['REQUEST_URI']);
         $category = ucfirst(end($arrUrl));
         $link = strtolower($category);
-        $params = ['itemList' => $this->items->findByCategory($category), 'title' => "Каталог",'link' => $link,
+        $params = [ 'title' => "Каталог",'link' => $link,
             'categoryList' => $categoryList, 'person' => $this->person, 'user' => $this->userInfo,
             'count' => $this->count ];
         $this->view->render('catalog', $params);
