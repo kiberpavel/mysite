e8302ba94d286dc3b19ca6e68dad38fe3c368404
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-22 18:52:06 +0300

    Add active record class

---
+++ Framework/core/ActiveRecordEntity.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Core;
+
+use Db\Database;
+
+abstract class ActiveRecordEntity
+{
+    public $id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function __set(string $name, $value)
+    {
+        $camelCaseName = $this->underscoreToCamelCase($name);
+        $this->$camelCaseName = $value;
+    }
+
+
+//    public function save(): void
+//    {
+//        $mappedProperties = $this->mapPropertiesToDbFormat();
+//        if ($this->id !== null) {
+//            $this->update($mappedProperties);
+//        } else {
+//            $this->insert($mappedProperties);
+//        }
+//    }
+
+    public static function convert($object)
+    {
+        $json = json_encode($object);
+        $objectToArray = json_decode($json, true);
+        return $objectToArray;
+    }
+
+
+    private function underscoreToCamelCase(string $source): string
+    {
+        return lcfirst(str_replace('_', '', ucwords($source, '_')));
+    }
+
+    private function camelCaseToUnderscore(string $source): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $source));
+    }
+
+//    private function mapPropertiesToDbFormat(): array
+//    {
+//        $reflector = new \ReflectionObject($this);
+//        $properties = $reflector->getProperties();
+//
+//        $mappedProperties = [];
+//        foreach ($properties as $property) {
+//            $propertyName = $property->getName();
+//            $propertyNameAsUnderscore = $this->camelCaseToUnderscore($propertyName);
+//            $mappedProperties[$propertyNameAsUnderscore] = $this->$propertyName;
+//        }
+//
+//        return $mappedProperties;
+//    }
+
+    public static function getById(int $id): ?self
+    {
+        $db = Database::getInstance();
+        $entities = $db->query(
+            'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
+            [':id' => $id],
+            static::class
+        );
+        return $entities ? $entities[0] : null;
+    }
+
+    public static function selectAll(): array
+    {
+        $db = Database::getInstance();
+        return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
+    }
+
+//    private function update(array $mappedProperties): void
+//    {
+//        $columns2params = [];
+//        $params2values = [];
+//        $index = 1;
+//        foreach ($mappedProperties as $column => $value) {
+//            $param = ':param' . $index; // :param1
+//            $columns2params[] = $column . ' = ' . $param; // column1 = :param1
+//            $params2values[$param] = $value; // [:param1 => value1]
+//            $index++;
+//        }
+//        $sql = 'UPDATE ' . static::getTableName() . ' SET ' . implode(', ', $columns2params) . ' WHERE id = ' . $this->id;
+//        $db = Database::getInstance();
+//        $db->query($sql, $params2values, static::class);
+//    }
+//
+//    private function insert(array $mappedProperties): void
+//    {
+//        $filteredProperties = array_filter($mappedProperties);
+//
+//        $columns = [];
+//        $paramsNames = [];
+//        $params2values = [];
+//        foreach ($filteredProperties as $columnName => $value) {
+//            $columns[] = '`' . $columnName . '`';
+//            $paramName = ':' . $columnName;
+//            $paramsNames[] = $paramName;
+//            $params2values[$paramName] = $value;
+//        }
+//
+//        $columnsViaSemicolon = implode(', ', $columns);
+//        $paramsNamesViaSemicolon = implode(', ', $paramsNames);
+//
+//        $sql = 'INSERT INTO ' . static::getTableName() . ' (' . $columnsViaSemicolon . ') VALUES (' . $paramsNamesViaSemicolon . ');';
+//
+//        $db = Database::getInstance();
+//        $db->query($sql, $params2values, static::class);
+//        $this->id = $db->getLastInsertId();
+//    }
+    public function delete(): void
+    {
+        $db = Database::getInstance();
+        $db->query(
+            'DELETE FROM `' . static::getTableName() . '` WHERE id = :id',
+            [':id' => $this->id]
+        );
+        $this->id = null;
+    }
+
+    abstract protected static function getTableName(): string;
+}
