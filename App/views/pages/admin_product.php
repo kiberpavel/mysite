de1e930301a21418f61a5dd215333f1c24ad6df2
Pavel Butenko <butenkobutenko2019@gmail.com>
2021-08-26 13:35:44 +0300

    Add admin functional

--- App/views/pages/admin_product.php
+++ App/views/pages/admin_product.php
@@ -1,9 +1,17 @@
 <main>
     <p class="title-name">Список товаров</p>
     <section class="admin-container">
-            <div class="add-product ">
-                    <a href="/admin/item/create">Добавить товар</a>
+                        <div class="product-container ">
+                                <a href="/admin/item/create" class="product">Добавить товар</a>
+                                    <a href="/admin/item" class="product">Все товары</a>
+                        </div>
+            <div>
+                    <form method="post" class="search">
+                            <input type="search" name="model" placeholder="поиск" class="input" />
+                            <input type="submit" name="" value="" class="submit" />
+                    </form>
             </div>
+
             <div class="item-admin">
         <?php foreach ($product as $item) : ?>
             <?php extract($item, EXTR_OVERWRITE); ?>
